2e01309a73d4f16e6484c98f210ca16d57979432
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-08-20 20:03:08 +0100

    style the header

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const divs = document.querySelectorAll("div")
 const regenerateButton = document.getElementById("regenerateButton")
 
+// Function to create random colors
 function regenerate() {
   for (const div of divs) {
     div.style.backgroundColor = `rgb(${Math.floor(256 * Math.random())}, ${Math.floor(256 * Math.random())}, ${Math.floor(256 * Math.random())})`;
   }
 }
 
+// Function to change color on click
+
+
+// Initial color regeneration
 regenerate()
+
+// Event listeners
 regenerateButton.addEventListener("click", regenerate)
 
