7237be78ef59650d28c8cf2417872973f0eed6b1
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-08-20 14:50:50 +0100

    link files together

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const regenerateButton = document.getElementById("regenerateButton")
 
 function regenerate() {
   for (const div of divs) {
-    div.style.backgroundColor = `rgb(${Math.floor(256 * Math.random())}, ${Math.floor(256 * Math.random())}, ${Math.floor(256 * Math.random())})`
+    div.style.backgroundColor = `rgb(${Math.floor(256 * Math.random())}, ${Math.floor(256 * Math.random())}, ${Math.floor(256 * Math.random())})`;
   }
 }
 
 regenerate()
 regenerateButton.addEventListener("click", regenerate)
+
