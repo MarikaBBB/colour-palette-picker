09c0621ea8788f3648c4e2cd47680fc7758605ec
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-08-20 13:12:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const divs = document.querySelectorAll("div")
+const regenerateButton = document.getElementById("regenerateButton")
+
+function regenerate() {
+  for (const div of divs) {
+    div.style.backgroundColor = `rgb(${Math.floor(256 * Math.random())}, ${Math.floor(256 * Math.random())}, ${Math.floor(256 * Math.random())})`
+  }
+}
+
+regenerate()
+regenerateButton.addEventListener("click", regenerate)
