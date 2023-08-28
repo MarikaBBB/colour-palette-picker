ecaabb1c7421a9c480e08d8019ea841f559f7821
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-08-28 15:39:26 +0100

    feat:generates a random color in hexadecimal format and display colour value

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
-const divs = document.querySelectorAll("div")
-const regenerateButton = document.getElementById("regenerateButton")
+const divs = document.querySelectorAll(".colour");
+const regenerateButton = document.getElementById("regenerateButton");
 
-// Function to create random colors
-function regenerate() {
-  for (const div of divs) {
-    div.style.backgroundColor = `rgb(${Math.floor(256 * Math.random())}, ${Math.floor(256 * Math.random())}, ${Math.floor(256 * Math.random())})`;
-  }
+// Function to generate a random color in hex format
+function generateRandomColorHex() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + randomColor.padStart(6, "0");
 }
 
-//Allow users to change each colour
+// Update colour code
+function updatePaletteColor(div, color) {
+  const colourInput = div.querySelector('.colour-input');
+  colourInput.value = color;
+  colourInput.style.backgroundColor = color;
+  div.style.backgroundColor = color;
+}
+
+// Allow users to change each colour
 divs.forEach((div) => {
-  let newColor;
+  const colourInput = div.querySelector('.colour-input');
+  const initialColor = generateRandomColorHex();
+  updatePaletteColor(div, initialColor);
   
   div.addEventListener('click', (event) => {
-    if (event.target.classList.contains('colour-input')) {
-      return; 
-    }
-
-    newColor = prompt('Enter a new color code:', '');
+    if (!event.target.classList.contains('colour-input')) {
+      const newColor = prompt('Enter a new color code:', '');
     
-    if (newColor !== null) {
-      div.style.backgroundColor = newColor;
-    } else {
-      alert('Invalid color code. Please enter a valid color code.');
-    }
+      if (newColor !== null) {
+        updatePaletteColor(div, newColor);
+      } else {
+        alert('Invalid color code. Please enter a valid color code.');
+      }
     
-    event.stopPropagation();
+      event.stopPropagation();
+    }
   });
 });
 
-
 // Initial color regeneration
-regenerate()
+function regenerateColors() {
+  divs.forEach((div) => {
+    const randomColor = generateRandomColorHex();
+    updatePaletteColor(div, randomColor);
+  });
+}
 
-// Event listeners
-regenerateButton.addEventListener("click", regenerate)
+// Initial color generation
+regenerateColors();
+
+// Event listener for regenerate button
+regenerateButton.addEventListener("click", regenerateColors);
+
 
