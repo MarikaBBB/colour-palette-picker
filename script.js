d8e276e3a07e80302aeabc3c878d71e8535b33ba
MarikaBBB <126022615+MarikaBBB@users.noreply.github.com>
2023-08-28 14:49:09 +0100

    feat: Add interactive color change functionality and Styling updates for the color divs and interactive elements

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const divs = document.querySelectorAll("div")
 const regenerateButton = document.getElementById("regenerateButton")
+const colourContainers = document.querySelectorAll('.colour');
 
 // Function to create random colors
 function regenerate() {
@@ -8,7 +9,26 @@ function regenerate() {
   }
 }
 
-// Function to change color on click
+//Allow users to change each colour
+divs.forEach((div) => {
+  let newColor;
+  
+  div.addEventListener('click', (event) => {
+    if (event.target.classList.contains('colour-input')) {
+      return; 
+    }
+
+    newColor = prompt('Enter a new color code:', '');
+    
+    if (newColor !== null) {
+      div.style.backgroundColor = newColor;
+    } else {
+      alert('Invalid color code. Please enter a valid color code.');
+    }
+    
+    event.stopPropagation();
+  });
+});
 
 
 // Initial color regeneration
